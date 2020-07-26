206770f211722bf494c21d4b3d38f43cc651649b
nainaistar <nainaistar@gmail.com>
2020-07-26 13:42:16 +0900

    [Add] github docsを使用するようにビルド

---
+++ frontend/build/precache-manifest.95ae8e51ac1e936c148f2badc9c08919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62b75544ec31e9fcf787a80a60d51f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f12bb2298ea0e13c3fea",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "61ce812e222a738841cf",
+    "url": "/static/js/2.65ab4ff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65ab4ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12bb2298ea0e13c3fea",
+    "url": "/static/js/main.376769d9.chunk.js"
+  },
+  {
+    "revision": "d049e954b1c74f29178c",
+    "url": "/static/js/runtime-main.9c7c50c1.js"
+  }
+]);
